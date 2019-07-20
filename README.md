f4c9d24bf1f32970df23fe2a9e8a0f3d260e34ed
Moremar <chevallier.th@gmail.com>
2019-07-20 12:14:05 +0900

    add a seed chooser + speed boost by creatingg only cells we need on the grid (instead of creating them all at startup)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # game_of_life
-Python GUI for John Conway's Game of Life
+
+This program is a Python TkInter GUI for John Conway's Game of Life.
+
+A 40x40 square of cells is randomly populated with cells from a given seed.
+The seed can be chosen to generate different evolution scenarios.
+
+The evolution of the cells can be observed either live or step by step.
--- game_of_life.py
+++ game_of_life.py
@@ -1,49 +1,49 @@
-from tkinter import *
+import tkinter as tk
 import math
 import random
 
 # colors
 WHITE = "white"
 BLACK = "black"
-GREY = "lightgrey"
-
+GREY  = "lightgrey"
+RED   = "red"
 
 # TK config
-X_CELLS = 200
-Y_CELLS = 125
-CELL_SIZE = 5
-SLEEP_TIME = 50  # time in ms between 2 moves
+X_CELLS    = 350
+Y_CELLS    = 200
+CELL_SIZE  = 4
+SLEEP_TIME = 10  # time in ms between 2 moves
+SEED_INIT  = 7
 
 
 class Cell:
-    def __init__(self, widget, x, y, color=WHITE):
+    def __init__(self, widget, x, y):
         self.widget = widget
         self.x = x
         self.y = y
-        self.color = color
 
 
-class App(Tk):
+class App(tk.Tk):
     def __init__(self, *args, **kwargs):
-        Tk.__init__(self, *args, **kwargs)
+        tk.Tk.__init__(self, *args, **kwargs)
         self.cells = dict()
         self.stopped = True    # flag to prevent to click multiple callbacks by Run click
         self.steps = 0
         self.alive = set()
 
         # Title
-        self.title_label = Label(self, text="John Conway's Game of Life", justify=CENTER)
+        self.title_label = tk.Label(self, text="John Conway's Game of Life", justify=tk.CENTER)
         self.title_label.pack()
 
         # Frame
-        self.frame = Frame(self, padx=1, pady=1, borderwidth=2, relief=GROOVE)
+        self.frame = tk.Frame(self, padx=1, pady=1, borderwidth=2, relief=tk.GROOVE)
         self.frame.pack()
 
         # Canvas Grid and squares
-        self.canvas = Canvas(self.frame,
-                             width=10 + X_CELLS * CELL_SIZE,
-                             height=10 + Y_CELLS * CELL_SIZE,
-                             bd=0, background=WHITE)
+        self.canvas = tk.Canvas(self.frame,
+                                width=10 + X_CELLS * CELL_SIZE,
+                                height=10 + Y_CELLS * CELL_SIZE,
+                                bd=0, background=WHITE)
         for i in range(0, 1 + X_CELLS):
             self.canvas.create_line(5 + CELL_SIZE * i, 5,
                                     5 + CELL_SIZE * i, 5 + Y_CELLS * CELL_SIZE,
@@ -54,54 +54,54 @@ class App(Tk):
                                     fill=GREY)
         self.canvas.pack()
 
-        # create cells
-        # TODO storing a rectangle for each cell is putting heavy pressure on the processor
-        # TODO it is fine for small grids but does not scale well
-        # TODO instead we could be a bit smarter and create only the rectangles we need
-        for i in range(0, X_CELLS):
-            for j in range(0, Y_CELLS):
-                cell = self.canvas.create_rectangle(
-                    5 + i * CELL_SIZE + 1, 5 + j * CELL_SIZE + 1,
-                    (i+1) * CELL_SIZE + 5, 5 + (j+1) * CELL_SIZE,
-                    fill=WHITE, outline="")
-                self.cells[(i, j)] = Cell(cell, i, j)
-
         # Start / Stop / Reset buttons
-        self.bottom_frame = Frame(self)
+        self.bottom_frame = tk.Frame(self)
         self.bottom_frame.pack()
-        self.next_button = Button(self, text="Next", command=self.next_button_click)
-        self.next_button.pack(side="left")
-        self.run_button = Button(self, text="Run", command=self.run_button_click)
-        self.run_button.pack(side="left")
-        self.stop_button = Button(self, text="Stop", command=self.stop_button_click)
-        self.stop_button.pack(side="left")
-        self.reset_button = Button(self, text="Reset", command=self.reset_button_click)
-        self.reset_button.pack(side="left")
+        self.next_button = tk.Button(self, text="Next", command=self.next_button_click)
+        self.next_button.pack(side=tk.LEFT, padx=(10, 1), pady=3)
+        self.run_button = tk.Button(self, text="Run", command=self.run_button_click)
+        self.run_button.pack(side=tk.LEFT, padx=1, pady=3)
+        self.stop_button = tk.Button(self, text="Stop", command=self.stop_button_click)
+        self.stop_button.pack(side=tk.LEFT, padx=1, pady=3)
+        self.reset_button = tk.Button(self, text="Reset", command=self.reset_button_click)
+        self.reset_button.pack(side=tk.LEFT, padx=1, pady=3)
+
+        # Seed for reset
+        self.seed_label = tk.Label(self, text="Seed : ")
+        self.seed_label.pack(side=tk.LEFT, padx=(20, 1), pady=3)
+        self.seed_textbox = tk.Entry(self, width=10)
+        self.seed_textbox.pack(side=tk.LEFT, padx=1, pady=3)
+        self.seed_textbox.insert(0, str(SEED_INIT))
+        self.seed_error_label = tk.Label(self, text="", fg=RED)
+        self.seed_error_label.pack(side=tk.LEFT, padx=1, pady=3)
 
         # steps count
-        self.steps_label1 = Label(self, text="Step : ")
-        self.steps_label2 = Label(self, text="0")
-        self.steps_label2.pack(side="right")
-        self.steps_label1.pack(side="right")
+        self.steps_label1 = tk.Label(self, text="Step : ")
+        self.steps_label2 = tk.Label(self, text="0")
+        self.steps_label2.pack(side=tk.RIGHT)
+        self.steps_label1.pack(side=tk.RIGHT)
+
+    def reset_seed(self):
+        seed = self.seed_textbox.get()
+        try:
+            seed = int(seed)
+            random.seed(seed)
+            self.seed_error_label.config(text=str(""))
+            return True
+        except ValueError:
+            self.seed_error_label.config(text=str("The seed must be an integer."))
+            return False
 
     def initial_setup(self):
-        random.seed(7)
-        # random.seed(222)        # stable in ~ 700 steps
-        # random.seed(3006)       # several ships, 1 crash, stable in ~1300 steps
-        cells = []
-
         # fill randomly some pixels in the 40x40 square in the center of the grid
-        # TODO make that configurable
+        cells = set()
         x0 = math.trunc(X_CELLS / 2)
         y0 = math.trunc(Y_CELLS / 2)
         for i in range(x0 - 20, x0 + 20):
             for j in range(y0 - 20, y0 + 20):
                 if random.randint(0, 1) > 0:
-                    cells.append((i, j))
-
-        for (x, y) in cells:
-            self.alive.add((x, y))
-            self.set_cell_color(x, y, BLACK)
+                    cells.add((i, j))
+        self.replace_cells(cells)
 
     def is_alive(self, x, y):
         return (x, y) in self.alive
@@ -118,10 +118,19 @@ class App(Tk):
                  (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)]
         return [(xi, yi) for (xi, yi) in cells if 0 <= xi < X_CELLS and 0 <= yi < Y_CELLS]
 
-    def set_cell_color(self, x, y, color):
+    def kill_cell(self, x, y):
+        # delete the black rectangle on the cell
         cell = self.cells[(x, y)]
-        cell.color = color                                  # logical color flip
-        self.canvas.itemconfigure(cell.widget, fill=color)  # physical color flip
+        self.canvas.delete(cell.widget)
+        del self.cells[(x, y)]
+
+    def create_cell(self, x, y):
+        # create a black rectangle on the cell
+        cell = self.canvas.create_rectangle(
+            5 + x * CELL_SIZE + 1, 5 + y * CELL_SIZE + 1,
+            (x + 1) * CELL_SIZE + 5, 5 + (y + 1) * CELL_SIZE,
+            fill=BLACK, outline="")
+        self.cells[(x, y)] = Cell(cell, x, y)
 
     # only alive cells and their neighbors are likely to change state
     def get_cells_to_check(self):
@@ -132,13 +141,13 @@ class App(Tk):
         return to_check
 
     def replace_cells(self, cells):
-        to_erase = self.alive.difference(cells)
-        to_draw = cells.difference(self.alive)
+        to_kill = self.alive.difference(cells)
+        to_create = cells.difference(self.alive)
         self.alive = cells
-        for (x, y) in to_erase:
-            self.set_cell_color(x, y, WHITE)
-        for (x, y) in to_draw:
-            self.set_cell_color(x, y, BLACK)
+        for (x, y) in to_kill:
+            self.kill_cell(x, y)
+        for (x, y) in to_create:
+            self.create_cell(x, y)
 
     def next_turn(self):
         to_check = self.get_cells_to_check()
@@ -177,8 +186,12 @@ class App(Tk):
             # waits 2 turn to ensure the main loop stopped
             self.after(2 * SLEEP_TIME, self.reset_button_click)
         else:
+            if not self.reset_seed():
+                # invalid seed, let the user fix it and click again on Reset
+                return
+
             for (x, y) in self.alive:
-                self.set_cell_color(x, y, WHITE)
+                self.kill_cell(x, y)
 
             self.alive = set()
             self.initial_setup()
@@ -188,5 +201,6 @@ class App(Tk):
 
 
 app = App()
+app.reset_seed()
 app.initial_setup()
 app.mainloop()
